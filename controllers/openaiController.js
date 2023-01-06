0421899f54b028d9fde452e7ee952651a3d64dfb
Steve-Tobias <sblaket@gmail.com>
2023-01-05 18:33:56 -0700

    modified: index.js, openaiController.js

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -7,9 +7,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async(req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    const {prompt, size} = req.body;
+
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
+
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: imageSize
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        });
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data)
+        } else {
+            console.log(error.message);
+        }
+
+        res.status(400).json({
+            success: false,
+            error: 'Hmm... Something went wrong generating your image!'
+        });
+    }
 };
 
 module.exports = {generateImage};
