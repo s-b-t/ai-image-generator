9ffbf40d677f6636aa96e72f5a6229bcfab1322d
Steve-Tobias <sblaket@gmail.com>
2023-01-05 17:53:28 -0700

    modified: index.js, openaiController.js, etc

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
 app.listen(port, () => console.log(`Server 🔥 up on PORT ${port}!`));
